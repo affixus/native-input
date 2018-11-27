5beca2557d768927999898b9b650d65b94a13ca4
santosh <santosh.ece06@gmail.com>
2018-11-27 16:48:31 +0530

    File upload.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NativeInput';
-  imageSource: any;
-  imgBlock: boolean;
+  imageSource1: any;
+  imageSource2: any;
+  
+  imgBlock1: boolean;
+  imgBlock2: boolean;
 
-  processImage(event) {
+  processImage(event, i) {
     if (event.target.files && event.target.files[0]) {
       const file = event.target.files[0];
 
       const reader = new FileReader();
       reader.onload = (e) => {
-        this.imgBlock = true;
-        this.imageSource = reader.result;
-      }
+
+        if(i===1) {
+          this.imgBlock1 = true;
+          this.imageSource1 = reader.result;
+        } else {
+          this.imgBlock2 = true;
+          this.imageSource2 = reader.result;
+        }
+      };
 
       reader.readAsDataURL(file);
     }
